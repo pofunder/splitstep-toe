2e2932a54f6a168708483c77320b606c9e84d3b5
pofunder <pofunder@gmail.com>
2025-04-22 17:50:54 -0700

    Update __init__.py

--- src/splitstep_toe/darkmatter/__init__.py
+++ src/splitstep_toe/darkmatter/__init__.py
@@ -1,1 +1,13 @@
+"""
+Dark-matter playground.
+
+Exports
+-------
+leapfrog_step  – symplectic 2-body Leapfrog
+demo_two_body  – evolve two equal-mass halos, return separation vs time
+"""
+
+from .leapfrog import leapfrog_step, demo_two_body
+
+__all__ = ["leapfrog_step", "demo_two_body"]
 
