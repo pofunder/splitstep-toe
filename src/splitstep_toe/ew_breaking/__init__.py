1a8fda25c197f46f20d3fb5dbd6a5f6eed48c80b
pofunder <pofunder@gmail.com>
2025-04-22 23:11:11 -0700

    Update __init__.py

--- src/splitstep_toe/ew_breaking/__init__.py
+++ src/splitstep_toe/ew_breaking/__init__.py
@@ -8,7 +8,10 @@ higgs_vev()  – returns 246 GeV
 from .higgs import higgs_vev
 
 __all__ = ["higgs_vev"]
-from .higgs import higgs_vev, lam_mu2_from_mh, higgs_mass    # noqa: F401
-__all__ = ["higgs_vev", "lam_mu2_from_mh", "higgs_mass"]
+"""
+Electroweak symmetry-breaking helpers
+"""
+
+from .higgs import higgs_vev, lam_mu2_from_mh, higgs_mass  # noqa: F401
 
 
