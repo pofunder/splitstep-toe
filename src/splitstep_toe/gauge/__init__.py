8e9c12b900c2f403fe5243fcc604c1ff9470c151
pofunder <pofunder@gmail.com>
2025-04-22 22:55:50 -0700

    Update __init__.py

--- src/splitstep_toe/gauge/__init__.py
+++ src/splitstep_toe/gauge/__init__.py
@@ -7,6 +7,8 @@ Currently:
 from .anomaly import anomaly_cancelled
 
 __all__ = ["anomaly_cancelled"]
-from .anomaly import anomaly_sums   # noqa: F401
+from .anomaly import anomaly_sums, anomaly_cancelled     # noqa: F401
+__all__ = ["anomaly_sums", "anomaly_cancelled"]
+
 
 
